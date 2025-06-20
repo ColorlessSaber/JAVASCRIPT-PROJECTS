20a8fb8eb3da6a9b0f76041557ae1c04ef0267f4
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-20 13:10:07 -0700

    Created a custom function that displays HTML text when button is pressed.

--- Basic JavaScript Projects/project2_functions/JS/main.js
+++ Basic JavaScript Projects/project2_functions/JS/main.js
@@ -1,5 +1,5 @@
 // A simple function that changes the text on the button when pressed.
-function combine_two_strings(){
+function combine_two_strings (){
     var str1 = "You clicked me!";
     var str2 = " YAY!"
     document.getElementById("button_text").innerHTML = str1 + str2;
@@ -10,4 +10,10 @@ function clickable_text (){
     var str1 = "Told ya "
     str1 += " that I was clickable!"
     document.getElementById("clickable_parag").innerHTML = str1;
+}
+
+//Function to display text when button is clicked
+function display_text() {
+    document.getElementById("message").innerHTML = "Should you be alive, if it's even possible to continue to exist in these sealed lands"
+                                                    + ", one day perhaps you will make atonement for what you've done."
 }
