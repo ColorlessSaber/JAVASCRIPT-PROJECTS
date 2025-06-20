69fbb005a2e197d0ba68d766b1c644b548b1fc59
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-20 11:17:50 -0700

    Added another line of code.

--- Basic JavaScript Projects/project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/project1_expressions_alert/JS/main.js
@@ -1,2 +1,6 @@
 //alert("Hello, world!");
-window.alert("Hello, world!")
+window.alert("Hello, world!");
+
+/*The function is still valid, dispite being called "deprected". its jsut consider bad practise becuase
+it does not work in XHTML.*/
+document.write("Hello, world!");
