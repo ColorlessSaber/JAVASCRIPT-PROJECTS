51e140e0445938f825441c3628e9a6b233c8cf7e
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-20 11:25:38 -0700

    Created a variable and wrote it on the screen via HTML.

--- Basic JavaScript Projects/project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/project1_expressions_alert/JS/main.js
@@ -4,3 +4,7 @@ window.alert("Hello, world!");
 /*The function is still valid, dispite being called "deprected". its jsut consider bad practise becuase
 it does not work in XHTML.*/
 document.write("Hello, world!");
+
+//Learning how to define a variable.
+var catch_phrase = "Sonic the Hedgehog! The fastest thing alive!";
+document.writeln(catch_phrase);
