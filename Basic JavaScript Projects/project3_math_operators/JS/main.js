c78331c74dc231c5e0a2dc3abde1834fd560b4f7
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-20 13:33:20 -0700

    Created a new HTML and JavaScript files to understand JS math.

---
+++ Basic JavaScript Projects/project3_math_operators/JS/main.js
@@ -0,0 +1,19 @@
+function add_result () {
+    var result = 3 + 5;
+    document.getElementById("math_add").innerHTML = "3 + 5 = " + result;
+}
+
+function sub_result () {
+    let result = 4 - 5;
+    document.getElementById("math_sub").innerHTML = "4 - 5 = " + result;
+}
+
+function div_result () {
+    let result = 64 / 8;
+    document.getElementById("math_div").innerHTML = "64 / 8 = " + result;
+}
+
+function multi_result () {
+    let result = 5 * 25;
+    document.getElementById("math_multi").innerHTML = "5 * 25 = " + result;
+}
