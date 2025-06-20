f8c24fee1ea81cbbb718f7ac799c22dfb940c9e7
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-20 14:19:59 -0700

    Finished with the math operators examples.

--- Basic JavaScript Projects/project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/project3_math_operators/JS/main.js
@@ -1,3 +1,4 @@
+//Below are functions that demastrate javascript's math operations
 function add_result () {
     var result = 3 + 5;
     document.getElementById("math_add").innerHTML = "3 + 5 = " + result;
@@ -21,4 +22,39 @@ function multi_result () {
 function math_operations () {
     let result = 70 / 70 * 70 + 70 - 70;
     document.getElementById("math_all").innerHTML = "70 / 70 * 70 + 70 - 70 = " + result;
+}
+
+function mod_op (){
+    let result = 125 % 8;
+    document.getElementById("mod_op_result").innerHTML = "The reminder is: " + result;
+}
+
+function neg_op (){
+    let result = -100;
+    document.getElementById("neg_op_result").innerHTML = "You get: " + -result;
+}
+// The end of functions that demastrate javascript's math operations.
+
+
+// Increment and decrement examples
+function increment_value (){
+    let parg_value = document.getElementById("number_value").innerText;
+    parg_value++;
+    document.getElementById("number_value").innerHTML = parg_value;
+}
+
+function decrement_value (){
+    let parg_value = document.getElementById("number_value").innerText;
+    parg_value--;
+    document.getElementById("number_value").innerHTML = parg_value;
+}
+
+// Utilizing a few Math functions
+function random_number (){
+    document.getElementById("show_random_number").innerHTML = Math.random() * 50;
+}
+
+function exp_number () {
+    let value = 5;
+    document.getElementById("exp_number_result").innerHTML = Math.exp(value);
 }
