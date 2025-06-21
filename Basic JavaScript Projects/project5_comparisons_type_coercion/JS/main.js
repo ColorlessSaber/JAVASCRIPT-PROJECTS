79c3651e9e4de73f6741d085654ac65c3be0a902
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-21 14:33:48 -0700

    Learning data types and JS's type coercion behavior.

---
+++ Basic JavaScript Projects/project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,5 @@
+// Using the typeof to learn how it works.
+document.write(typeof 10.5269);
+
+// Testing out type coercion and see how JS will respond.
+document.write(70 / "70" * "70" + "70" - 70); // THis will print 7000. 
