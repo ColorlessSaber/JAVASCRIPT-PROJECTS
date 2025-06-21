a340ea52ff7b49ccde7413b72a50088b412a7fdc
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-21 14:37:05 -0700

    Wrote a note to myself to understand how the type coercion worked.

--- Basic JavaScript Projects/project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/project5_comparisons_type_coercion/JS/main.js
@@ -2,4 +2,6 @@
 document.write(typeof 10.5269);
 
 // Testing out type coercion and see how JS will respond.
-document.write(70 / "70" * "70" + "70" - 70); // THis will print 7000. 
+document.write(70 / "70" * "70" + "70" - 70);
+// This will print 7000. Why? It does 70/70, which is 1 and 1 time 70 is just 70. Next it appends 70 to 70 to get 7070,
+// and when you subtract 70 from 7070 you get 7000.
