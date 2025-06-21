d0f1d63e2118d9583faa65409ca4c53f768285a2
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-21 14:22:56 -0700

    learned how to make and use JS dictionaries.

---
+++ Basic JavaScript Projects/project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function favorite_icecream() {
+    var icecream_flavors = {
+        flavor1: "Oreo Cookie Ice Cream",
+        flavor2: "Vinilla Ice Cream",
+        flavor3: "Mini Chocolate Chip Ice Cream",
+        flavor4: "Cookie & Cream Ice Cream"
+    };
+    delete icecream_flavors.flavor3;
+    document.getElementById("dictionary").innerHTML = "My favorite ice cream today is: " + icecream_flavors.flavor3;
+}
