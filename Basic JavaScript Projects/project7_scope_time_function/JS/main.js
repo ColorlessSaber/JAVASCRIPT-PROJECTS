43cf7d27e843a2ec4fb942ff05ecd20f8eec0ee4
ColorlessSaber <110745660+ColorlessSaber@users.noreply.github.com>
2025-06-22 13:27:21 -0700

    Added comments to JS code, and added an if-else-if statement.

--- Basic JavaScript Projects/project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/project7_scope_time_function/JS/main.js
@@ -14,7 +14,7 @@ function village2 () {
 village1();
 village2();
 
-// If statement example
+// If statement examples
 function check_the_time () {
     if (new Date().getHours() > 12) {
         document.getElementById("time_check").innerHTML = "Yes indeedy! It is past noon!";
@@ -22,10 +22,27 @@ function check_the_time () {
 }
 
 function allowed_to_pull_sword () {
+    // Check to see if Link (AKA hero) is pulling the Master Sword.
     if (document.getElementById("name").value == "Link") {
         document.getElementById("sword_pull_result").innerHTML = "You are the Hero! You can pull the Master Sword out!";
     }
     else {
         document.getElementById("sword_pull_result").innerHTML = "You are not the Hero. You cannot pull the Master Sword out.";
     }
+}
+
+function time_function () {
+    // gets the local time of user and sees if it is morning, afternoon, or evening.
+    var time = new Date().getHours();
+    var reply;
+    if (time < 12 == time > 0) {
+        reply = "It is morning time!";
+    }
+    else if (time >= 12 == time < 18) {
+        reply = "It is afternoon.";
+    }
+    else {
+        reply = "It is evening time.";
+    }
+    document.getElementById("time_of_day").innerHTML = reply;
 }
